b4b07879cc483c34e81256d98730f4834a7c9f54
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-07-27 17:10:15 -0500

    will output a nice csv

--- heatmap_rsids_samples.py
+++ heatmap_rsids_samples.py
@@ -11,7 +11,6 @@ bwa_mask  = "S%02d.bwa_ir_fb.norepeats.intersected.vep.filtered.csv"
 directory = "/export/home/rgarcia/MCAC/vcf/intersections/filtered/"
 
 
-score_threshold = 5
 
 varcount = {}
 
@@ -23,33 +22,28 @@ for ID in IDs.all_ids:
             vepreader = csv.reader(csvfile, delimiter='\t')
             header = vepreader.next()
             for row in vepreader:
-                if int(row[0]) >= score_threshold:
-                    existing = row[7].split(',')
-                    if ID in existing:
-                        if sample in varcount[ID]:
-                            varcount[ID][sample] += 1
-                        else:
-                            varcount[ID][sample] = 1
+                existing = row[7].split(',')
+                if ID in existing:
+                    if sample in varcount[ID]:
+                        varcount[ID][sample] += 1
+                    else:
+                        varcount[ID][sample] = 1
     
+rows = []
+for ID in IDs.all_ids:
+    cols = []
+    for sample in range(1, 49):
+        if sample in varcount[ID]:
+            cols.append(1)
+        else:
+            cols.append(0)
+    rows.append(cols)
 
-pprint(varcount)
-                                            
-# rows = []
-# for sample in range(1, 49):
-#     cols = []
-#     for gene in genes:
-#         if gene in varcount[sample]:
-#             heat = len(varcount[sample][gene])
-#         else:
-#             heat = 0
-#         cols.append(heat)
-#     rows.append(cols)
-
-
-# with open('var_count_per_sample_score_gt5.csv', 'wb') as csvfile:
-#     varwriter = csv.writer(csvfile, delimiter=',')
-#     varwriter.writerow(['S'] + genes )
-#     for row in range(len(rows)):
-#         varwriter.writerow([row+1] + rows[row] )
-    
+with open('var_count_per_existing_variation_gt5.csv', 'wb') as csvfile:
+    varwriter = csv.writer(csvfile, delimiter=',')
+    varwriter.writerow(['S'] + range(1,49) )
+    row = 0
+    for ID in IDs.all_ids:
+        varwriter.writerow([ID] + rows[row] )
+        row += 1
 
