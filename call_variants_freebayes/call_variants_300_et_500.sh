8600e9d57a688ac375d9ee2da9113b3b8e858f61
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-05 00:08:07 -0500

    a for, new sets

---
+++ call_variants_freebayes/call_variants_300_et_500.sh
@@ -0,0 +1,28 @@
+FREEBAYES=/usr/local/bin/freebayes
+REF=/home/rgarcia/MCAC/reference/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFASTA/genome.fa
+BED=/home/rgarcia/MCAC/targets/Miocardiopatias_y_canalopatias_AllTracks.bed
+
+# SAMDIR=/home/rgarcia/MCAC/sam/300
+# VCFDIR=/home/rgarcia/MCAC/vcf/300
+
+# mkdir -p $VCFDIR
+
+# for SAMPLE in $(seq -f "%02g" 1 48)
+# do
+#     $FREEBAYES --standard-filters -X -u  -t $BED -f $REF $SAMDIR/S${SAMPLE}_realigned.bam -v $VCFDIR/S${SAMPLE}.vcf &
+# done
+
+
+
+
+
+
+SAMDIR=/home/rgarcia/MCAC/sam/500
+VCFDIR=/home/rgarcia/MCAC/vcf/500
+
+mkdir -p $VCFDIR
+
+for SAMPLE in $(seq -f "%02g" 1 48)
+do
+    $FREEBAYES --standard-filters -X -u  -t $BED -f $REF $SAMDIR/S${SAMPLE}_realigned.bam -v $VCFDIR/S${SAMPLE}.vcf &
+done
