c871cdde94349e728c001efdf88186b8fc524d6a
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-05 00:09:03 -0500

    convenience script

---
+++ vep_etl/drop_tables.sql
@@ -0,0 +1,13 @@
+drop table colocated_variants;
+drop table downstream_gene_variant;
+drop table intron_variant;
+drop table missense_variant;
+drop table nmd_transcript_variant;
+drop table non_coding_transcript_exon_variant;
+drop table non_coding_transcript_variant;
+drop table regulatory_features;
+drop table splice_region_variant;
+drop table synonymous_variant;
+drop table three_prime_UTR_variant;
+drop table upstream_gene_variant;
+drop table variants;
