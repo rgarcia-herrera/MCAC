2f432922af64f10d48696309e7114d51202faaa1
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-05 00:05:34 -0500

    new set

--- alignment_map_prepare/add_rg.sh
+++ alignment_map_prepare/add_rg.sh
@@ -1,62 +1,27 @@
 #!/bin/bash
 
-DIR=/export/home/rgarcia/MCAC/sam_miseq_hg19
-ADDORREPLACEREADGROUPS=/export/home/rgarcia/software/picard-tools-1.119/AddOrReplaceReadGroups.jar
+DIR=/home/rgarcia/MCAC/sam
+ADDORREPLACEREADGROUPS=/home/rgarcia/downloads/picard-tools-1.121/AddOrReplaceReadGroups.jar
 
-
-BAMS="/export/home/rgarcia/MCAC/sam_miseq_hg19/S01.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S02.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S03.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S04.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S05.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S06.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S07.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S08.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S09.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S10.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S11.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S12.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S13.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S14.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S15.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S16.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S17.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S18.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S19.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S20.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S21.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S22.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S23.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S24.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S25.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S26.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S27.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S28.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S29.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S30.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S31.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S32.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S33.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S34.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S35.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S36.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S37.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S38.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S39.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S40.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S41.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S42.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S43.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S44.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S45.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S46.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S47.bam
-/export/home/rgarcia/MCAC/sam_miseq_hg19/S48.bam"
-
-for BAM in $BAMS
+for SAMPLE in $(seq -f "%02g" 1 48)           
 do
-    BAMOUT=$DIR/`basename $BAM .bam`_rg.bam
-    SAMPLE=`basename $BAM .bam`
+    BAM=$DIR/300/S${SAMPLE}.bam
+    BAMOUT=$DIR/300/S${SAMPLE}_rg.bam
+    java -jar $ADDORREPLACEREADGROUPS \
+                 I=$BAM \
+                 LB='mcac' \
+                 PL='illumina' \
+                 PU='nil' \
+                 SM=$SAMPLE \
+                 O=$BAMOUT
+
+done
+
+
+for SAMPLE in $(seq -f "%02g" 1 48)           
+do
+    BAM=$DIR/500/S${SAMPLE}.bam
+    BAMOUT=$DIR/500/S${SAMPLE}_rg.bam
     java -jar $ADDORREPLACEREADGROUPS \
                  I=$BAM \
                  LB='mcac' \
