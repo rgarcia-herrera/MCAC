cbeabe31fb8e17e6c84c994e6b6e7b05d6cb518e
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-07 11:45:31 -0500

    fastqc on raw data

---
+++ quality_control/fastqc.sh
@@ -0,0 +1,16 @@
+FASTQC=/home/rgarcia/downloads/FastQC/fastqc
+
+FASTQDIR=/home/rgarcia/MCAC/fastq
+FASTQCDIR=/home/rgarcia/MCAC/qc/fastqc
+
+mkdir -p $FASTQCDIR
+
+for SAMPLE in $(seq -f "%02g" 1 48)
+do
+    for SIZE in {300,500}
+    do
+        OUTDIR=$FASTQCDIR/$SIZE/S${SAMPLE}
+        $FASTQC -t 30 $FASTQDIR/$SIZE/S${SAMPLE}_R1.fastq.gz $FASTQDIR/$SIZE/S${SAMPLE}_R2.fastq.gz -o $OUTDIR
+    done
+done
+
