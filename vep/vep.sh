a3760795dd545f4047c398926562b94032f7ad39
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-07 15:21:02 -0500

    nice embedded fors

--- vep/vep.sh
+++ vep/vep.sh
@@ -1,5 +1,13 @@
-VEP=~/software/ensembl-tools-release-79/scripts/variant_effect_predictor/variant_effect_predictor.pl
+#!/bin/bash
 
-$VEP -i vcf/bwa_fb/S24.norepeats.vcf -o S01.json --json --everything --fork 24 --offline --assembly GRCh37
+VEP=/home/rgarcia/downloads/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl
 
+VCFDIR=/home/rgarcia/MCAC/vcf
 
+for SAMPLE in $(seq -f "%02g" 1 48)
+do
+    for BATCH in {300,500,300et500}
+    do
+        $VEP -i $VCFDIR/$BATCH/S${SAMPLE}.norepeats.vcf --json -o $VCFDIR/$BATCH/S${SAMPLE}.vep.json --everything --fork 30 --offline --assembly GRCh37
+    done
+done
