73e36b481ce8e8ef41f61333836440d446da22e5
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-07 12:59:10 -0500

    for each 300 and 500

--- quality_control/qualimap_html.sh
+++ quality_control/qualimap_html.sh
@@ -4,9 +4,18 @@ QUALIMAP=/home/rgarcia/downloads/qualimap_v2.1.1/qualimap
 BASE=/home/rgarcia/MCAC
 BED=$BASE/targets/Miocardiopatias_y_canalopatias_Amplicons.bed
 
-BAMDIR=$BASE/sam/sam_miseq_hg19
-QCDIR=$BASE/qc/qualimap
+BAMDIR=$BASE/sam/300
+QCDIR=$BASE/qc/qualimap/300
 mkdir -p $QCDIR
 
-find $BAMDIR -iname 'S*_rg_realigned.bam' \
+find $BAMDIR -iname 'S*_realigned.bam' \
+     -printf "$QUALIMAP bamqc --feature-file $BED -c -outformat HTML -bam %p -outdir $QCDIR/%f \n" 
+
+
+
+BAMDIR=$BASE/sam/500
+QCDIR=$BASE/qc/qualimap/500
+mkdir -p $QCDIR
+
+find $BAMDIR -iname 'S*_realigned.bam' \
      -printf "$QUALIMAP bamqc --feature-file $BED -c -outformat HTML -bam %p -outdir $QCDIR/%f \n" 
