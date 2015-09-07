8b66dd3c692866de2b33bf71ac47050f185c6540
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-07 11:45:45 -0500

    concurrent call from 300 and 500 reads

--- call_variants_freebayes/call_variants_300_et_500.sh
+++ call_variants_freebayes/call_variants_300_et_500.sh
@@ -17,12 +17,25 @@ BED=/home/rgarcia/MCAC/targets/Miocardiopatias_y_canalopatias_AllTracks.bed
 
 
 
-SAMDIR=/home/rgarcia/MCAC/sam/500
-VCFDIR=/home/rgarcia/MCAC/vcf/500
+# SAMDIR=/home/rgarcia/MCAC/sam/500
+# VCFDIR=/home/rgarcia/MCAC/vcf/500
+
+# mkdir -p $VCFDIR
+
+# for SAMPLE in $(seq -f "%02g" 1 48)
+# do
+#     $FREEBAYES --standard-filters -X -u  -t $BED -f $REF $SAMDIR/S${SAMPLE}_realigned.bam -v $VCFDIR/S${SAMPLE}.vcf &
+# done
+
+
+
+
+SAMDIR=/home/rgarcia/MCAC/sam
+VCFDIR=/home/rgarcia/MCAC/vcf/300et500
 
 mkdir -p $VCFDIR
 
 for SAMPLE in $(seq -f "%02g" 1 48)
 do
-    $FREEBAYES --standard-filters -X -u  -t $BED -f $REF $SAMDIR/S${SAMPLE}_realigned.bam -v $VCFDIR/S${SAMPLE}.vcf &
+    $FREEBAYES --standard-filters -X -u  -t $BED -f $REF $SAMDIR/300/S${SAMPLE}_realigned.bam $SAMDIR/500/S${SAMPLE}_realigned.bam -v $VCFDIR/S${SAMPLE}.vcf &
 done
