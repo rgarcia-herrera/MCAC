bea234eeab2708e74353b0bba2eadcf968fe737c
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-09-13 01:46:58 -0500

    vep for family filtered complete_g vcfs

--- vep/vep.sh
+++ vep/vep.sh
@@ -4,10 +4,28 @@ VEP=/home/rgarcia/downloads/ensembl-tools-release-81/scripts/variant_effect_pred
 
 VCFDIR=/home/rgarcia/MCAC/vcf
 
-for SAMPLE in $(seq -f "%02g" 1 48)
+
+
+
+# for SAMPLE in $(seq -f "%02g" 1 48)
+# do
+#     for BATCH in {300,500,300et500}
+#     do
+#         $VEP -i $VCFDIR/$BATCH/S${SAMPLE}.norepeats.vcf --json -o $VCFDIR/$BATCH/S${SAMPLE}.vep.json --everything --fork 30 --offline --assembly GRCh37
+#     done
+# done
+
+
+
+
+
+COMPLETE_VCF="family_filtered_PTLiJATEiJIAT-JPAMuLACA_con_alelos.vcf
+family_filtered_PTLiJATEiJIAT-JPAMuLACA_sin_alelos.vcf
+family_filtered_PTLiJATE-JPAMuLACA_con_alelos.vcf
+family_filtered_PTLiJATE-JPAMuLACA_sin_alelos.vcf"
+
+for VCF in $COMPLETE_VCF
 do
-    for BATCH in {300,500,300et500}
-    do
-        $VEP -i $VCFDIR/$BATCH/S${SAMPLE}.norepeats.vcf --json -o $VCFDIR/$BATCH/S${SAMPLE}.vep.json --everything --fork 30 --offline --assembly GRCh37
-    done
+    OUT=`basename $VCF .vcf`
+    $VEP -i $VCFDIR/complete/$VCF --json -o $VCFDIR/complete/${OUT}.vep.json --everything --fork 30 --offline --assembly GRCh37    
 done
