eec9e44a21fe00f0bbbc5feba08c85c1b6eb0c05
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-12-01 19:41:08 -0600

    effect prediction on complete genomics intersection

--- vep/vep.sh
+++ vep/vep.sh
@@ -5,33 +5,26 @@ VEP=/home/rgarcia/downloads/ensembl-tools-release-81/scripts/variant_effect_pred
 VCFDIR=/home/rgarcia/MCAC/vcf
 
 
-
-
 # for SAMPLE in $(seq -f "%02g" 1 48)
 # do
-#     for BATCH in {300,500,300et500}
+#     for BATCH in {500,}
 #     do
-#         $VEP -i $VCFDIR/$BATCH/S${SAMPLE}.norepeats.vcf --json -o $VCFDIR/$BATCH/S${SAMPLE}.vep.json --everything --fork 30 --offline --assembly GRCh37
+#         $VEP -i $VCFDIR/$BATCH/S${SAMPLE}.norepeats.vcf -o $VCFDIR/$BATCH/S${SAMPLE}.vep.csv --everything --fork 30 --offline --assembly GRCh37
 #     done
 # done
 
 
 
+COMPLETE_VCF="family_filtered_PTLiJATEiJIAT-JPAMuLACA.vcf
+family_filtered_PTLiJATE-JPAMuLACA.vcf
+family_filtered_PTLiJATEiJIAT.vcf
+family_filtered_PTLiJATE.vcf"
 
-
-# COMPLETE_VCF="family_filtered_PTLiJATEiJIAT-JPAMuLACA_con_alelos.vcf
-# family_filtered_PTLiJATEiJIAT-JPAMuLACA_sin_alelos.vcf
-# family_filtered_PTLiJATE-JPAMuLACA_con_alelos.vcf
-# family_filtered_PTLiJATE-JPAMuLACA_sin_alelos.vcf"
-
-# for VCF in $COMPLETE_VCF
-# do
-#     OUT=`basename $VCF .vcf`
-#     $VEP -i $VCFDIR/complete/$VCF --json -o $VCFDIR/complete/${OUT}.vep.json --everything --fork 30 --offline --assembly GRCh37    
-# done
-
-
-
+for VCF in $COMPLETE_VCF
+do
+    OUT=`basename $VCF .vcf`
+    $VEP -i $VCFDIR/complete/$VCF -o $VCFDIR/complete/${OUT}.vep.csv --everything --fork 30 --offline --assembly GRCh37    
+done
 
 
 # EXM_ILLUMINA_VCF="family_filtered_JLRLiJLRMiGEL_con_alelos.vcf
@@ -39,20 +32,25 @@ VCFDIR=/home/rgarcia/MCAC/vcf
 # family_filtered_JLRLiJLRMiGEL_sin_alelos.vcf
 # family_filtered_JLRMiYRM_sin_alelos.vcf"
 
-# EXM_ILLUMINA_VCF="DLA.vcf
-# GEL.vcf
-# GMCA.vcf
-# JLRL.vcf
-# PTL.vcf"
+# EXM_ILLUMINA_VCF="LMG.vcf
+# JLRM.vcf
+# YRN.vcf"
+# # DLA.vcf
+# # GEL.vcf
+# # GMCA.vcf
+# # JLRL.vcf
+# # PTL.vcf
+# # LMG.vcf"
 
 # for VCF in $EXM_ILLUMINA_VCF
 # do
 #     OUT=`basename $VCF .vcf`
-#     $VEP -i $VCFDIR/exm_illumina/$VCF --json -o $VCFDIR/exm_illumina/${OUT}.vep.json --everything --fork 30 --offline --assembly GRCh37    
+#     $VEP --force_overwrite -i $VCFDIR/exm_illumina/$VCF -o $VCFDIR/exm_illumina/${OUT}.vep.csv --everything --fork 30 --offline --assembly GRCh37    
 # done
 
 
-for SAMPLE in $(seq -f "%02g" 1 17)
-do
-    $VEP -i $VCFDIR/caem_fvi_rigo/S${SAMPLE}.vcf --json -o $VCFDIR/caem_fvi_rigo/S${SAMPLE}.vep.json --everything --fork 30 --offline --assembly GRCh37
-done
+
+#for SAMPLE in $(seq -f "%02g" 1 11)
+#do
+#    $VEP -i $VCFDIR/caem_fvi_rigo/S${SAMPLE}.vcf -o $VCFDIR/caem_fvi_rigo/S${SAMPLE}.vep.csv --everything --fork 30 --offline --assembly GRCh37
+#done
