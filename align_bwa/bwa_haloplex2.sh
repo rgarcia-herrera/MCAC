5761738320a1cdc5f11c2b78b445bd3e911e2def
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-12-01 19:43:47 -0600

    just like another day in the farm

---
+++ align_bwa/bwa_haloplex2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=/home/rgarcia/MCAC
+BWA=/home/rgarcia/downloads/bwa.kit/bwa
+REF=$DIR/reference/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFASTA/genome.fa
+
+
+for SAMPLE in $(seq -f "%02g" 1 48)
+do
+    R1=$DIR/fastq/haloplex2/S${SAMPLE}_R1.fastq.gz
+    R2=$DIR/fastq/haloplex2/S${SAMPLE}_R2.fastq.gz
+    SAMOUT=$DIR/sam/haloplex2/S${SAMPLE}.sam
+    $BWA mem -M -t 30 $REF $R1 $R2 > $SAMOUT
+done
+
