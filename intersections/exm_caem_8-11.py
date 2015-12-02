ff2270ed8f60207ac4f1d0411627b98fc3caa8a4
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-12-01 19:43:26 -0600

    nice intersections

---
+++ intersections/exm_caem_8-11.py
@@ -0,0 +1,31 @@
+import vcf
+from pprint import pprint
+
+vcfdir = '/home/rgarcia/MCAC/vcf/caem_fvi_rigo'
+
+samples = [ 'S08','S11']
+
+vars = {}
+for sample in samples:
+    path = "%s/%s.vcf" % (vcfdir, sample)
+    f = open(path, 'r')
+    vars[sample] = set()
+    vcfr = vcf.Reader( f )
+    for v in vcfr:
+        variant = "%s:%s %s/%s" % (v.CHROM, v.POS, v.REF, v.ALT)
+        vars[sample].add(variant)
+
+        
+interesting = vars['S08'] - vars['S11']
+
+
+with open( '%s/S01.vcf' % vcfdir, 'r' ) as inhandle, \
+     open('%s/family_filtered_8-11.vcf' % vcfdir, 'w') as outhandle:
+    invcf   = vcf.Reader( inhandle )
+    outvcf  = vcf.Writer( outhandle, invcf )
+
+    for v in invcf:
+        variant = "%s:%s %s/%s" % (v.CHROM, v.POS, v.REF, v.ALT)
+        if variant in interesting:
+            outvcf.write_record(v)    
+
