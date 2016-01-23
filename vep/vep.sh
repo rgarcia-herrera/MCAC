63b12d1d68438599af33b11e7776a39dd76be26f
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-01-22 23:41:56 -0600

    why not VEP the basespace set?

--- vep/vep.sh
+++ vep/vep.sh
@@ -78,13 +78,72 @@ VCFDIR=/home/rgarcia/MCAC/vcf
 
 
 #A01.JATE.vcf"
-COMPLETE_COMPLETE="B01.PTL.vcf
-C01.JPAM.vcf
-D01.LACA.vcf
-E01.JIAT.vcf"
+# COMPLETE_COMPLETE="B01.PTL.vcf
+# C01.JPAM.vcf
+# D01.LACA.vcf
+# E01.JIAT.vcf"
 
-for VCF in $COMPLETE_COMPLETE
+# for VCF in $COMPLETE_COMPLETE
+# do
+#     OUT=`basename $VCF .vcf`
+#     $VEP -i $VCFDIR/complete/$VCF -o $VCFDIR/complete/${OUT}.vep.csv --everything --fork 30 --offline --assembly GRCh37
+# done
+
+
+
+
+haloplexbasespace="11_S1.vcf
+12_S1.vcf
+13_S1.vcf
+14_S1.vcf
+15_S1.vcf
+16_S1.vcf
+17_S1.vcf
+18_S1.vcf
+19_S1.vcf
+20_S1.vcf
+21_S1.vcf
+22_S1.vcf
+23_S1.vcf
+24_S1.vcf
+25_S1.vcf
+26_S1.vcf
+27_S1.vcf
+28_S1.vcf
+29_S1.vcf
+30_S1.vcf
+31_S1.vcf
+32_S1.vcf
+33_S1.vcf
+34_S1.vcf
+35_S1.vcf
+36_S1.vcf
+37_S1.vcf
+38_S1.vcf
+39_S1.vcf
+40_S1.vcf
+41_S1.vcf
+42_S1.vcf
+43_S1.vcf
+44_S1.vcf
+45_S1.vcf
+46_S1.vcf
+47_S1.vcf
+48_S1.vcf
+CFOG_S1.vcf
+CYDR_S1.vcf
+EEPO_S1.vcf
+FJGA_S1.vcf
+IXH_S1.vcf
+JCVM_S1.vcf
+JDJM_S1.vcf
+JOS_S1.vcf
+JSMC_S1.vcf
+MJG_S1.vcf
+RRR_S1.vcf"
+
+for VCF in $haloplexbasespace
 do
     OUT=`basename $VCF .vcf`
-    $VEP -i $VCFDIR/complete/$VCF -o $VCFDIR/complete/${OUT}.vep.csv --everything --fork 30 --offline --assembly GRCh37
+    $VEP -i $VCFDIR/haloplex500basespace/$VCF -o $VCFDIR/haloplex500basespace/${OUT}.vep.csv --everything --fork 30 --offline --assembly GRCh37
 done
