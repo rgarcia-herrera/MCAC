a93a4df6d070ffad632f7822025c2bfa732fc92c
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-01-22 23:42:33 -0600

    add sample to annotated variant document

--- shared_variation/load_rvs_mongo.py
+++ shared_variation/load_rvs_mongo.py
@@ -4,21 +4,21 @@ from pprint import pprint
 from samples import vcf_paths
 from rvs_ann import rvs_annotate
 
-cases = {}
+variants = {}
 for sample in vcf_paths:
-    cases[sample] = set()
+    variants[sample] = set()
     vcfr = vcf.Reader( open(vcf_paths[sample], 'r') )
 
     for v in vcfr:
         if v.is_snp:
             variant = (v.CHROM.replace('chr',''), v.POS, str(v.ALT[0]))
-            cases[sample].add(variant)
+            variants[sample].add(variant)
 
-pprint( rvs_annotate([("1",955553,"C"), ("7",140434407,"A")]) )
 
-# for sample in cases:
-#     print sample
-#     print rvs_annotate(cases[sample])
-#     for resource in rvs_annotate(cases[sample])
+for sample in variants:
+    for v in rvs_annotate(variants[sample]):
+        v['sample'] = sample
+        # load to mongo
+
 
 
