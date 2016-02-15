22ffff8a84ca462c893c8cc3fea3370ce2c9801f
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-02-15 17:31:18 -0600

    dont compute groupaf

--- allele_frequencies/groupAF_vs_gmaf.py
+++ allele_frequencies/groupAF_vs_gmaf.py
@@ -1,48 +1,55 @@
-import csv
+import vcf
 import os
 from pprint import pprint
 
-from samples import vep_probands
+from samples import vcf_probands
 
 
-cases = {}
-for path in vep_probands:
+variants = {}
+
+for path in vcf_probands:
     with open(path) as f:
-        csvr = csv.reader(f, delimiter='\t')
-        sample = os.path.basename(path)
-        cases[sample] = set()
-        for v in csvr:
-            if not v[0].startswith('#'):
+        vcfr = vcf.Reader(f)
+        sample = vcfr.samples[0]
 
-                extraslist = v[13].split(';')
-                extras = {}
-                for e in extraslist:
-                    (key,value) = e.split('=')
-                    extras[key] = value
+        for v in vcfr:
+            if 'CSQ' in v.INFO:
+                for csq in v.INFO['CSQ']:
+                    (Allele,Consequence,IMPACT,SYMBOL,Gene,Feature_type,Feature,
+                     BIOTYPE,EXON,INTRON,HGVSc,HGVSp,cDNA_position,CDS_position,
+                     Protein_position,Amino_acids,Codons,Existing_variation,DISTANCE,
+                     STRAND,VARIANT_CLASS,SYMBOL_SOURCE,HGNC_ID,CANONICAL,TSL,CCDS,
+                     ENSP,SWISSPROT,TREMBL,UNIPARC,SIFT,PolyPhen,DOMAINS,GMAF,
+                     AFR_MAF,AMR_MAF,ASN_MAF,EAS_MAF,EUR_MAF,SAS_MAF,AA_MAF,EA_MAF,
+                     CLIN_SIG,SOMATIC,PHENO,PUBMED,MOTIF_NAME,MOTIF_POS,HIGH_INF_POS,MOTIF_SCORE_CHANGE) = csq.split('|')
 
-                if 'GMAF' in extras:
-                    rsids = v[12]
-                    gmafs = extras['GMAF'].split(',')
-                    for gmaf in gmafs:
-                        variant = (extras.get('SYMBOL','-'), rsids, v[1], gmaf)
-                        cases[sample].add(variant)
+                    chrom = v.CHROM
+                    if chrom.find('chr') == -1:
+                        chrom = 'chr' + chrom
+                    
+                    if GMAF:
+                        vkey = "%s:%s_%s/%s" % (chrom,v.POS,v.REF,v.ALT)
+                        if not vkey in variants:
+                            variants[vkey] = {'symbol'  : SYMBOL,
+                                              'existing': Existing_variation,
+                                              'gmaf'    : GMAF,
+                                              'het'     : set(),
+                                              'hom'     : set(),
+                            }
+
+                        if v.num_het == 0:
+                            variants[vkey]['hom'].add(sample)
+                        else:
+                            variants[vkey]['het'].add(sample)
 
 
-union     = set.union( *cases.values() )
+print ";".join(["vkey","symbol","existing","gmaf","het","hom"])
 
-frequencies = {v: 0 for v in union}
-
-for v in union:
-    for sample in cases:
-        if v in cases[sample]:
-            frequencies[v]+=1
-
-grouplen = len(vep_probands) * 2
-
-print ";".join(["symbol","keys","location","alt","gmaf","groupaf"])
-
-for v in frequencies:
-    groupaf = float(frequencies[v])/float(grouplen*2)
-    (alt,gmaf) = v[3].split(':')
-    print ";".join([v[0],v[1],v[2],str(alt),str(gmaf),str(groupaf)])
-
+for v in variants:
+    print ";".join([v,
+              variants[v]['symbol'],
+              variants[v]['existing'],
+              variants[v]['gmaf'],
+              ",".join(variants[v]['het']),
+              ",".join(variants[v]['hom'])])
+              
